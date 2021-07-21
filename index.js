37933e13dcae32c6d59468824588eef87886bf71
Sam Shearer <sshearer101@gmail.com>
2021-07-21 10:53:57 -0600

    commit tree 35d7fdd4a4eb70651d285ad904f43d8656ec335e

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+
+  if (number <= 400) {
+    return 'This one is on me!'
+  }
+  else if (number < 2500 & number > 2000) {
+    return 'I will gladly take your thirty bucks.'
+   }
+   else {
+    return 'No can do.'
+   }
+ }
+
+function ternaryCheckCity(city){
+  const newYork = city == 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return newYork
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  let tipMoney;
+
+  switch (money) {
+    case "generous":
+      tipMoney = "Thank you so much."
+      break;
+    case "not as generous":
+      tipMoney = "Thank you."
+      break;
+    default:
+      tipMoney = "Bye."
+      
+  }
+  return tipMoney
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
